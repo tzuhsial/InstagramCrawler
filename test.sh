7024b8997a81a9479a0c7fd2f0b8827900197b96
Antonie Lin <iammrhelo@gmail.com>
2017-07-07 20:23:05 +0800

    setup firefox environment

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
+echo "Check photos & captions..."
 echo "Download first 10 photos and captions of account 'instagram'"
 python instagramcrawler.py -q 'instagram' -n 20 -c
 
+echo "Check hashtag..."
 echo "Search for hashtag '#breakfast' and download 20 photos"
 python instagramcrawler.py -q '#breakfast' -n 20
